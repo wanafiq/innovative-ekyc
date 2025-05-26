0a6414c6f5450923289f8e0aa34452b1afe48de9
Wan <105415014+wanafiq91@users.noreply.github.com>
2025-05-26 17:01:34 +0800

    init

---
+++ src/main/java/com/wan/innovative_ekyc/InnovativeEkycApplication.java
@@ -0,0 +1,13 @@
+package com.wan.innovative_ekyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovativeEkycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovativeEkycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wan/innovative_ekyc/InnovativeEkycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wan.innovative_ekyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovativeEkycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
